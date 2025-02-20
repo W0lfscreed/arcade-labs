76dd6679528d150acf8646a136e6ec5cabcc9e55
W0lfscreed <pedro.julian@edu.uah.es>
2025-02-20 20:40:29 +0100

    betrayal house

--- lab06-adventure/lab_06.py
+++ lab06-adventure/lab_06.py
@@ -2,11 +2,11 @@
 
 class Room:
     def __init__(self, description, north, south, east, west):
-        self.description = ""
-        self.north = 0
-        self.south = 0
-        self.east = 0
-        self.west = 0
+        self.description = description
+        self.north = north
+        self.south = south
+        self.east = east
+        self.west = west
 
 def main():
 
@@ -14,31 +14,62 @@ def main():
 
     #Corridors
 
-    corridor1= Room("Room", "North", None, None, "West")
+    corridor1= Room("You are on a dimly lit corridor.", "North", None, None, "West")
     room_list.append(corridor1)
 
-    corridor2 = Room("Room", "North", "South", "East", "West")
+    corridor2 = Room("A long, narrow hallway.", "North", "South", "East", "West")
     room_list.append(corridor2)
 
-    corridor3= Room("Room", None, "South", "East", None)
+    corridor3= Room("A cold, stone passage.", None, "South", "East", None)
     room_list.append(corridor3)
 
     #Rooms
 
-    room1 = Room("Room", "North", None, "East", None)
+    room1 = Room("A small room with old furniture.", "North", None, "East", None)
     room_list.append(room1)
 
-    room2 = Room("Room", None, "South", "East", None)
+    room2 = Room("A dusty chamber.", None, "South", "East", None)
     room_list.append(room2)
 
-    room3 = Room("Room", "North", None, None, "West")
+    room3 = Room("A mysterious library.", "North", None, None, "West")
     room_list.append(room3)
 
-    room4 = Room("Room", None, "South", None, "West")
+    room4 = Room("A torch-lit chamber.", None, "South", None, "West")
     room_list.append(room4)
 
-    current_room = 0
-    print(room_list)
+    current_room = room_list[0]
+
+    print(current_room.description)
+
+    done = False
+    while not done:
+        done=True
+
+    print()
+
+    direction = str(input("Which direction would you like to go? "))
+
+    if  direction == "North" or "n" or "N" or "NorRth":
+        direction= "North"
+        next_room = room_list[current_room].north
+
+    elif direction == "South" or "s" or "S" or "Soutrth":
+        direction= "South"
+        next_room = room_list[current_room].south
+
+    elif direction == "East" or "e" or "E" or "East":
+        direction= "East"
+        next_room = room_list[current_room].east
+
+    elif direction == "West" or "w" or "W" or "West":
+        direction= "West"
+        next_room = room_list[current_room].west
+
+    else:
+        print("You can’t go that way.")
+        next_room = room_list[current_room]
+
 
 
 main()
+
